41569f77cbb26f72aa735f68993bd5230c36b7b6
ramnivas11 <sutharramnivas10@gmail.com>
2026-07-12 11:03:10 +0530

    Initial backend setup

---
+++ assetflow-backend/src/app.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import compression from "compression";
+import cookieParser from "cookie-parser";
+import healthRoutes from "./routes/health.routes";
+import { requestLogger } from "./middlewares/logger.middleware";
+import { notFound } from "./middlewares/notFound.middleware";
+import { errorHandler } from "./middlewares/error.middleware";
+
+const app = express();
+
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    })
+);
+
+app.use(helmet());
+app.use(requestLogger);
+app.use(compression());
+app.use("/api/v1/health", healthRoutes);
+app.use("/api/v1/db", dbRoutes);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(notFound);
+
+app.use(errorHandler);
+
+export default app;
